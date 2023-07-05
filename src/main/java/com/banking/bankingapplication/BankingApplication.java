b1a83c27ea7fbbf899d3cd66e3af76853aa1620d
Hamza Bouachir <hbouachir@dom.tti>
2023-07-05 14:55:50 +0100

    prntln

--- src/main/java/com/banking/bankingapplication/BankingApplication.java
+++ src/main/java/com/banking/bankingapplication/BankingApplication.java
@@ -23,7 +23,7 @@ public class BankingApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankingApplication.class, args);
 
-
+        System.out.println("hello hamzaaaaaaaaaaaaaaaaaaaaaaaaaaaawi");
 
     }
 
